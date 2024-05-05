a36984e5b636311b88aa04b59fc6bfc02dea8a88
Heeyeong <gmldud5659@naver.com>
2024-05-05 18:40:30 +0900

    :memo: 기초문제 Day 9 부분 문자열 이어 붙여 문자열 만들기

---
+++ basicQuestion/Day_9/Main2.java
@@ -0,0 +1,29 @@
+import java.util.LinkedList;
+
+/**
+ * 코딩 기초 트레이닝 Day 9
+ * 배열 만들기 5
+ */
+public class Main2 {
+    public static void main(String[] args) {
+        int[] result = solution(new String[]{"0123456789","9876543210","9999999999999"}, 50000, 5, 5);
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static int[] solution(String[] intStrs, int k, int s, int l) {
+        LinkedList<Integer> list = new LinkedList<>();
+
+        for (int i = 0; i < intStrs.length; i++) {
+            int tmp = Integer.parseInt(intStrs[i].substring(s, s + l));
+
+            if (tmp > k) {
+                list.add(tmp);
+            }
+        }
+
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
