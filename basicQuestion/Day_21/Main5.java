f6a7906594ebf18f51bbee57ddc6eae04143682b
Heeyeong <gmldud5659@naver.com>
2024-06-17 22:05:51 +0900

    :memo: 기초문제 Day 21 문자열을 정수로 변환하기

---
+++ basicQuestion/Day_21/Main5.java
@@ -0,0 +1,14 @@
+/**
+ * 코딩 기초 트레이닝 Day 21
+ * 문자열을 정수로 변환하기
+ */
+public class Main5 {
+    public static void main(String[] args) {
+        System.out.println(solution("10"));
+        System.out.println(solution("8542"));
+    }
+
+    public static int solution(String n_str) {
+        return Integer.parseInt(n_str);
+    }
+}
