928d73ee4fcb642b31c7ccedf51a8df127e05c9c
Heeyeong <gmldud5659@naver.com>
2024-06-17 21:57:01 +0900

    :memo: 기초문제 Day 21 정수 부분

---
+++ basicQuestion/Day_21/Main3.java
@@ -0,0 +1,14 @@
+/**
+ * 코딩 기초 트레이닝 Day 21
+ * 정수 부분
+ */
+public class Main3 {
+    public static void main(String[] args) {
+        System.out.println(solution(1.42));
+        System.out.println(solution(69.32));
+    }
+
+    public static int solution(double flo) {
+        return (int) flo;
+    }
+}
