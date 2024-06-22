f110ca856c01875d54c514ebf8f3025c5f252408
Heeyeong <gmldud5659@naver.com>
2024-06-22 20:30:57 +0900

    :memo: 입문문제 Day 1 두 수의 곱

---
+++ introductionQuestion/Day_1/Main3.java
@@ -0,0 +1,14 @@
+/**
+ * 코딩테스트 입문 Day 1 사칙연산
+ * 두 수의 곱
+ */
+public class Main3 {
+    public static void main(String[] args) {
+        System.out.println(solution(3, 4));
+        System.out.println(solution(27, 19));
+    }
+
+    public static int solution(int num1, int num2) {
+        return num1 * num2;
+    }
+}
