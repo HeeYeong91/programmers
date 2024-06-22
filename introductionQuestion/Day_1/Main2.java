251336b176ec5871016b76c3086d35fd6548a2e0
Heeyeong <gmldud5659@naver.com>
2024-06-22 20:29:32 +0900

    :memo: 입문문제 Day 1 두 수의 차

---
+++ introductionQuestion/Day_1/Main2.java
@@ -0,0 +1,14 @@
+/**
+ * 코딩테스트 입문 Day 1 사칙연산
+ * 두 수의 차
+ */
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println(solution(2, 3));
+        System.out.println(solution(100, 2));
+    }
+
+    public static int solution(int num1, int num2) {
+        return num1 - num2;
+    }
+}
