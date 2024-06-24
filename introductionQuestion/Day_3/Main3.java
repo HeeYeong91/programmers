6b829990c175f109f9742c9147fc6390cd3a7cfc
Heeyeong <gmldud5659@naver.com>
2024-06-24 09:06:43 +0900

    :memo: 입문문제 Day 3 최빈값 구하기

---
+++ introductionQuestion/Day_3/Main3.java
@@ -0,0 +1,39 @@
+/**
+ * 코딩테스트 입문 Day 3 사칙연산, 배열, 수학
+ * 최빈값 구하기
+ */
+public class Main3 {
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{1, 2, 3, 3, 3, 4}));
+        System.out.println(solution(new int[]{1, 1, 2, 2}));
+        System.out.println(solution(new int[]{1}));
+    }
+
+    public static int solution(int[] array) {
+        int answer = 0;
+        int max = 0;
+        int[] frequent = new int[1000];
+
+        for (int i = 0; i < array.length; i++) {
+            frequent[array[i]]++;
+
+            if (max < frequent[array[i]]) {
+                max = frequent[array[i]];
+                answer = array[i];
+            }
+        }
+        
+        int count = 0;
+        for (int i = 0; i < 1000; i++) {
+            if (max == frequent[i]) {
+                count++;
+            }
+
+            if (count > 1) {
+                return -1;
+            }
+        }
+
+        return answer;
+    }
+}
