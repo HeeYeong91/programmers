a3b990be8c9bcd18eafafeb65fdc8cc0bd3a7e6f
Heeyeong <gmldud5659@naver.com>
2024-07-02 09:50:30 +0900

    :memo: 입문문제 Day 10 공 던지기

---
+++ introductionQuestion/Day_10/Main3.java
@@ -0,0 +1,15 @@
+/**
+ * 코딩테스트 입문 Day 10 조건문, 배열, 수학, 시뮬레이션
+ * 2차원으로 만들기
+ */
+public class Main3 {
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{1, 2, 3, 4}, 2));
+        System.out.println(solution(new int[]{1, 2, 3, 4, 5, 6}, 5));
+        System.out.println(solution(new int[]{1, 2, 3}, 3));
+    }
+
+    public static int solution(int[] numbers, int k) {
+        return numbers[2 * (k - 1) % numbers.length];
+    }
+}
